af1f7fbffff2abaf6001c9d86c3720251c847781
prabhuwebdev <h.prabhu2dev2@gmail.com>
2025-04-15 16:10:40 +0530

    added the first model

---
+++ School_management/school_management_addon/models/fee_assignment.py
@@ -0,0 +1,9 @@
+from odoo import models,fields
+
+class FeeAssignment(models.Model):
+    _name = 'school_management.fee.assignment'
+    _description = 'Fee Assignment'
+
+    fee_structure_id = fields.Many2one('fee.structure', string='Fee Structure')
+    fee_category_id = fields.Many2one('fee.category', string='Fee Category')
+    amount = fields.Float(string='Amount', default=0.0)
