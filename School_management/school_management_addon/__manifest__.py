45876f43f41f5ec39bba3fbd69ea403cad4eb2e7
prabhuwebdev <h.prabhu2dev2@gmail.com>
2025-04-15 15:19:23 +0530

    second commit

---
+++ School_management/school_management_addon/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name":"School Management",
+    "version":"1.0",
+    "summary":"This is about the School Management Project",
+    "author":"Jvl Techonologies",
+    "category":"School Management",
+    "depends":['base',"web"],
+    "data":[
+
+    ],
+    'assets':{
+
+    },
+    "installable":True,
+    "application":True,
+
+}
